4da693a42de6fee259e0f7402648c9714b259539
mahbub hasan <mahbubhasan2001d7@gmail.com>
2022-02-28 23:28:28 +0600

    React-JS-Series-Challenge-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='text-left'>
+        <h1>Mahbub Hasan Welcome JSk Challenge </h1>
+        <p>List of top 5 series</p>
+        <ol>
+          <li>Texas Chainsaw Massacre. Image via Netflix.</li>
+          <li>Despicable Me 2. Image via Universal. </li>
+          <li>St. Vincent.</li>
+          <li>The Tinder Swindler. Image via Netflix.</li>
+          <li>Downfall: The Case Against Boeing. Image via Sundance.</li>
+        </ol>
+        </div>
   );
 }
 
